679bdee47fbcaa47f7929a77bda082f5829cded6
Malino <adatx100@gmail.com>
2023-08-15 10:30:21 +0700

    update ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Loading from "./screens/Loading/Loading";
+
 import PlayGame from "./screens/Game/PlayGame";
 import Intron from "./screens/Game/Introduce";
 import GameCard from "./screens/Game/GameCard";
 import Point from "./screens/Game/Point";
 import Flipcard from "./screens/Game/Flipcard";
+import Loading from "./screens/Loading/Loading";
 
 function App() {
   return (
--- src/screens/Loading/Loading.tsx
+++ src/screens/Loading/Loading.tsx
@@ -1,97 +1,81 @@
-import { useCallback, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import appConfig from "../../config";
-import gamefoxSDK from "../../gamefoxSDK";
-import {
-  useConfig,
-  useInventory,
-  useQuest,
-  useUser,
-  useItems,
-  useSystemInfo,
-  usePool,
-} from "../../hooks";
-// import { ASSETS } from '../../utils/assetUtils';
-import classes from "./loading.module.css";
+import { BarLoader } from "react-spinners";
+import { useState, useEffect } from "react";
 
-type Props = {
-  //
-};
-
-export const LoadingScreen: React.FC<Props> = () => {
-  const navigate = useNavigate();
-  const [inited, setInited] = useState(false);
-  const [loadedAssets, setLoadedAssets] = useState(false);
-
-  const [, setUser] = useUser();
-  const [, getConfig] = useConfig();
-  const [, getInventory] = useInventory();
-  const [, getItems] = useItems();
-  const [, getQuests] = useQuest();
-  const [, getSystemInfo] = useSystemInfo();
-  const [, getPool] = usePool();
-
-  const init = useCallback(async () => {
-    // TODO: use configuration set from sdk
-    try {
-      if (appConfig.online) {
-        await gamefoxSDK.init(appConfig.apiUrl);
-
-        getConfig();
-        const user = await gamefoxSDK.auth();
-        setUser(user);
-        await getInventory();
-        await getItems();
-        await getQuests();
-        await getSystemInfo();
-        await getPool();
-      } else {
-        // Offline usecase
-        setUser({
-          _id: "userId",
-          data: {
-            username: "Nguyen Van Dat",
-          },
-        });
-        getConfig();
-        await getInventory();
-        await getItems();
-        await getQuests();
-        await getSystemInfo();
-        await getPool();
-      }
-      setInited(true);
-    } catch (error: any) {
-      if (error?.message) {
-      }
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+export default function Loading() {
+  const [isLoading, setIsLoading] = useState(true);
+  const containerStyle = {
+    backgroundImage: "url('../src/assets/backgroup.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "100vw",
+    height: "101vh",
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      window.location.href = "/play";
+    }, 5000);
+    return () => clearTimeout(timer);
   }, []);
+  if (isLoading) {
+    return (
+      <div
+        style={containerStyle}
+        className="flex flex-col items-center justify-center"
+      >
+        <img
+          src="../src/assets/shinhan.png"
+          alt="shinhan"
+          className="w-[10rem]"
+          style={{ position: "absolute", top: "30px" }}
+        />
+        <img
+          src="../src/assets/textloading.png"
+          alt="textloading"
+          className="w-[50rem]"
+          style={{ position: "absolute", top: "96px" }}
+        />
 
-  useEffect(() => {
-    if (!inited) {
-      init();
-    }
-  }, [init, inited]);
-
-  useEffect(() => {
-    if (inited && loadedAssets) {
-      console.log("navigate");
-      navigate("/game", {
-        replace: true,
-      });
-    }
-  }, [navigate, inited, loadedAssets]);
-
-  return (
-    <div className={classes.container}>
-      {/* <img src={ASSETS.LOADING_BG} alt="" className={classes['background']} /> */}
-      {/* <img src={ASSETS.SOL_LOGO} alt="" className={classes['logo']} /> */}
-      <div className={classes["loadingWrapper"]}>
-        <span className="pro-regular">Chờ chút nhé!</span>
-        <div className={classes["loadingBar"]}></div>
+        <img
+          src="../src/assets/maybe.png"
+          alt="maybe"
+          className="w-[9rem] ml-36 "
+          style={{ position: "absolute", top: "270px" }}
+        />
+        <img
+          src="../src/assets/bird.png"
+          alt="maybe"
+          className="w-[5rem] "
+          style={{ position: "absolute", top: "290px", left: "120px" }}
+        />
+        <img
+          src="../src/assets/logobackgroup.png"
+          alt="logobackgroup"
+          className="text-center  w-[24em]"
+          style={{ position: "absolute", top: "370px" }}
+        />
+        <div
+          className="w-[60vw] "
+          style={{ position: "absolute", top: "540px" }}
+        >
+          <BarLoader
+            color="#848CEE"
+            className=" ml-5"
+            width={280}
+            height={10}
+            cssOverride={{
+              borderRadius: 50,
+              backgroundColor: "white",
+              marginLeft: "-10px",
+            }}
+          />
+          <h1 className="text-center text-2xs ml-3" style={{ color: "#fff" }}>
+            Chờ chút nhé!
+          </h1>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  } else {
+    return null;
+  }
+}
